6219a205bc892cf05284d38eeb22aa284c5130b7
kasso <ondrej.kassovic@posam.sk>
2022-11-07 23:02:53 +0100

    refactor scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, VERSION } from '@angular/core'
-import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AlzaInv'
--- src/app/hero/pages/list-heroes/list-heroes.component.ts
+++ src/app/hero/pages/list-heroes/list-heroes.component.ts
@@ -3,12 +3,10 @@ import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'app-list-heroes',
   templateUrl: './list-heroes.component.html',
-  styleUrls: ['./list-heroes.component.css'],
+  styleUrls: ['./list-heroes.component.scss'],
 })
 export class ListHeroesComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    console.log('somt u?')
-  }
+  ngOnInit(): void {}
 }
